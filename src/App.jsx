fba2466a4339ca42b57454781f961bac9866208c
Tomas Storc <tomas.storc@gmail.com>
2024-03-02 22:33:29 +0100

    diff

--- src/App.jsx
+++ src/App.jsx
@@ -35,21 +35,8 @@ function App() {
         setDbRes(data.data);
         const end = new Date();
         const diff = end - start;
-        const SEC = 1000,
-          MIN = 60 * SEC,
-          HRS = 60 * MIN;
-        const humanDiff = `${Math.floor(diff / HRS)}:${Math.floor(
-          (diff % HRS) / MIN
-        ).toLocaleString("en-US", { minimumIntegerDigits: 2 })}:${Math.floor(
-          (diff % MIN) / SEC
-        ).toLocaleString("en-US", { minimumIntegerDigits: 2 })}.${Math.floor(
-          diff % SEC
-        ).toLocaleString("en-US", {
-          minimumIntegerDigits: 4,
-          useGrouping: false,
-        })}`;
-        setTime(humanDiff);
-        console.log(humanDiff, dbRes);
+        setTime(diff);
+        console.log(diff, dbRes);
       });
   };
   return (
