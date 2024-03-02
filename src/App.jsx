2e6216cf1fe673ada0aed7ab1ad9aa991854adc2
Tomas Storc <tomas.storc@gmail.com>
2024-03-02 22:19:58 +0100

    add selector

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,19 @@ const URL =
 const columns = [
   {
     name: "ID",
+    selector: (row) => row.id,
   },
   {
     name: "First name",
+    selector: (row) => row.first_name,
   },
   {
     name: "Last name",
+    selector: (row) => row.last_name,
   },
   {
     name: "email",
+    selector: (row) => row.email,
   },
 ];
 
