b61681235b699952fae70afc9b9f5b01c0a91768
Tomas Storc <tomas.storc@gmail.com>
2024-03-03 16:58:53 +0100

    get correct time

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ function App() {
   const [time, setTime] = useState();
   const [dbRes, setDbRes] = useState([]);
   const handleClick = () => {
-    const start = new Date();
+    const start = new Date().getTime();
     fetch(URL)
       .then((res) => res.json())
       .then((data) => {
         console.log(data.data, "returned data");
         setDbRes(data.data);
-        const end = new Date();
+        const end = new Date().getTime();
         const diff = end - start;
         setTime(diff);
         console.log(diff, dbRes);
@@ -23,7 +23,7 @@ function App() {
   return (
     <>
       <button onClick={handleClick}>Získat data</button>
-      <div style={{ color: "black" }}>celkový čas {time}</div>
+      <div style={{ color: "black" }}>celkový čas: {time}</div>
       <DataTable columns={columns} data={dbRes} />
     </>
   );
